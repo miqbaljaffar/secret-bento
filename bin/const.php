06d0624db76fe98d642c8052453966e2ecc079de
miqbaljaffar <iqbaljaffar1108@gmail.com>
2025-08-27 11:37:05 +0700

    fix folder

--- bin/const.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_SERVER', '127.0.0.1:3306'); //"mysql324.phy.lolipop.lan"
-define('DB_USERNAME', 'root'); //"LAA1611970"
-define('DB_PASSWORD', 'Iqbal#0811'); //"Mhu1FNyK"
-define('DB_NAME_HANDOVER', 'LAA1611970-bento');
-
-?>
